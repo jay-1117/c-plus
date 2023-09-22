c1c3b81457cb69e40346437a17827e55d290ddab
Jay Patel <132971309+jay-1117@users.noreply.github.com>
2023-09-22 22:35:51 +0530

    Add files via upload

---
+++ area of  triangle.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//int l;
+//cout<<"enter the value of l:";
+//cin>>l;
+//
+//
+//cout<<"\narea of square="<<l*l;
+//}
+
+//are of circle:-
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+float pie=3.14,r;
+
+cout<<"Enter the value of radius:";
+cin>>r;
+
+cout<<"\narea of circle="<<pie*r*r;
+}
+
